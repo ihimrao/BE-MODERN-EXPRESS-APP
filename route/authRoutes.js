3a246272abc1c8877cffc0dc8907dffe36eb91a3
imsarkar <hraj0105@gmail.com>
2022-06-09 13:07:48 +0530

    1 file modified

--- route/authRoutes.js
+++ route/authRoutes.js
@@ -1,11 +1,11 @@
 const { root, assignToken } = require('../controller/authController');
-
+const auth = require('../middleware/authMiddleware');
 module.exports = (function () {
   'use strict';
 
   var authRoutes = require('express').Router();
 
-  authRoutes.get('/', root);
+  authRoutes.get('/', auth, root);
 
   return authRoutes;
 })();
